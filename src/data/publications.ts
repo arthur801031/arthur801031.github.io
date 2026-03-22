121cdbdacaacc6f478e06c8788395c3095fdda90
arthur801031 <arthur801031@gmail.com>
2026-03-21 18:00:12 -0700

    Use smaller GIFs

--- src/data/publications.ts
+++ src/data/publications.ts
@@ -17,7 +17,7 @@ export const publications: Publication[] = [
       "<u>I-Chun Arthur Liu</u>, Krzysztof Choromanski, Sandy Huang, Connor Schenck",
     venue: "Under Review",
     year: 2025,
-    gif: "/gifs/clamp.gif",
+    gif: "/gifs/clamp_small.gif",
     links: [
       { label: "arXiv", url: "https://arxiv.org/abs/2602.00937" },
     ],
@@ -42,7 +42,7 @@ export const publications: Publication[] = [
       "<u>I-Chun Arthur Liu</u>, Jason Chen, Gaurav S. Sukhatme, Daniel Seita",
     venue: "Conference on Robot Learning (CoRL), 2025",
     year: 2025,
-    gif: "/gifs/d-coda.gif",
+    gif: "/gifs/d-coda_small.gif",
     links: [
       { label: "arXiv", url: "https://arxiv.org/abs/2505.04860" },
       { label: "Code", url: "https://github.com/dcodaaug/dcoda" },
@@ -56,7 +56,7 @@ export const publications: Publication[] = [
       "<u>I-Chun Arthur Liu</u>, Sicheng He, Daniel Seita<strong>†</strong>, Gaurav S. Sukhatme<strong>†</strong>",
     venue: "Conference on Robot Learning (CoRL), 2024",
     year: 2024,
-    gif: "/gifs/voxact-b.gif",
+    gif: "/gifs/voxact-b_small.gif",
     links: [
       { label: "arXiv", url: "https://arxiv.org/pdf/2407.04152v1" },
       { label: "Code", url: "https://github.com/VoxAct-B/voxactb" },
@@ -74,7 +74,7 @@ export const publications: Publication[] = [
       "Gautam Salhotra<strong>*</strong>, <u>I-Chun Arthur Liu</u><strong>*</strong>, Gaurav S. Sukhatme",
     venue: "Conference on Robot Learning (CoRL), 2023",
     year: 2023,
-    gif: "/gifs/mail.gif",
+    gif: "/gifs/mail_small.gif",
     links: [
       { label: "arXiv", url: "https://arxiv.org/abs/2304.03833" },
       { label: "Code", url: "https://github.com/uscresl/mail" },
@@ -92,7 +92,7 @@ export const publications: Publication[] = [
       "Gautam Salhotra<strong>*</strong>, <u>I-Chun Arthur Liu</u><strong>*</strong>, Marcus Dominguez-Kuhne, Gaurav S. Sukhatme",
     venue: "IEEE Robotics and Automation Letters (RA-L) & IROS 2022",
     year: 2022,
-    gif: "/gifs/dmfd.gif",
+    gif: "/gifs/dmfd_small.gif",
     links: [
       { label: "arXiv", url: "https://arxiv.org/abs/2207.10148" },
       { label: "Code", url: "https://github.com/uscresl/dmfd" },
@@ -111,7 +111,7 @@ export const publications: Publication[] = [
       "<u>I-Chun Arthur Liu</u><strong>*</strong>, Shagun Uppal<strong>*</strong>, Gaurav S. Sukhatme, Joseph J. Lim, Peter Englert, Youngwoon Lee",
     venue: "Conference on Robot Learning (CoRL), 2021",
     year: 2021,
-    gif: "/gifs/mopa-pd.gif",
+    gif: "/gifs/mopa-pd_small.gif",
     links: [
       {
         label: "arXiv",
